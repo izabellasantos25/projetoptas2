eaf8be89620aa38d8b860e9bfb1ce37e074ab778
izabellasantos25 <izabella.miranda@estudante.ifms.edu.br>
2023-06-05 17:26:49 -0400

    commit dois

--- index.js
+++ index.js
@@ -25,3 +25,23 @@ app.get('/pessoas', async function(req, res){
 app.listen(3000, function() {
   console.log('App de Exemplo escutando na porta 3000!')
 });
+
+app.post('/pessoas', async function(req, res){
+  try {
+    var pessoas = await Pessoa.insert(req.body);
+    res.json(pessoas.rows);
+  } catch (error) {
+    console.error('Erro ao buscar pessoas:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar pessoas' });
+  }
+});
+
+app.delete('/pessoas', async function(req, res){
+  try {
+    var pessoas = await Pessoa.delete(req.body.id);
+    res.json(pessoas.rows);
+  } catch (error) {
+    console.error('Erro ao buscar pessoas:', error);
+    res.status(500).json({ error: 'Ocorreu um erro ao buscar pessoas' });
+  }
+});
--- models/pessoa.js
+++ models/pessoa.js
@@ -4,7 +4,7 @@ class Pessoa {
   static async select() {
     try {
       const connect = await db.connect();
-      const sql = "select * from pessoas;"
+      const sql = "select * from pessoas";
       return await connect.query(sql);
     } catch (error) {
       console.error('Erro em select:', error);
@@ -15,7 +15,7 @@ class Pessoa {
   static async insert(data) {
     try {
       const connect = await db.connect();
-      const sql = "";
+      const sql = "INSERT INTO pessoas (nome, idade, uf) VALUES ($1, $2, $3);";
       const values = [data.nome, data.idade, data.uf];
       return await connect.query(sql, values);
     } catch (error) {
@@ -39,7 +39,7 @@ class Pessoa {
   static async delete(id) {
     try {
       const connect = await db.connect();
-      const sql = "";
+      const sql = "delete from pessoas where id = $1";
       return await connect.query(sql, [id]);
     } catch (error) {
       console.error('Erro em delete:', error);
